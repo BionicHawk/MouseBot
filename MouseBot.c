7efaa0f430ce8455ddebb6c2f69f63e937b30291
BionicHawk <116476139+BionicHawk@users.noreply.github.com>
2023-04-12 19:40:54 -0600

    App Launcher
    This C file is ready to be compiled to generate a Launcher as an exe file

---
+++ MouseBot.c
@@ -0,0 +1,10 @@
+// Desarrollado por Angel de Jesús Manzo Rosas (Alias = BionicHawk)
+#include <stdio.h>
+
+//Esta parte del código ejecuta el comando de python para abrir la aplicación
+int main(){
+    printf("Iniciando Interprete");
+    system("python MouseBot.py");
+    system("pause");
+    return 0;
+}
